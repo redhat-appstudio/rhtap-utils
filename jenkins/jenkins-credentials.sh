c3f84bb724c692aa00595dab4fdb50c44e8493e3
jsmid1 <jsmid@redhat.com>
2024-11-08 15:49:05 +0100

    [RHTAP-3262] Adds GitOps credentials

--- jenkins/jenkins-credentials.sh
+++ jenkins/jenkins-credentials.sh
@@ -10,17 +10,20 @@ COSIGN_PASSWORD=$PASSWORD cosign generate-key-pair
 COSIGN_SECRET_PASSWORD="$(base64 -w0 <<< $PASSWORD)"
 COSIGN_SECRET_KEY="$(base64 -w0 < cosign.key)"
 COSIGN_PUBLIC_KEY="$(base64 -w0 < cosign.pub)"
+GITOPS_AUTH_USERNAME="<GitOps username>"
 GITOPS_GIT_TOKEN="<GitOps token>"
+QUAY_USERNAME="<Quay username>"
+QUAY_PASSWORD="<Quay password>"
 ACS_TOKEN="<ACS token>"
 ACS_ENDPOINT="<ACS endpoint>"
 
 
 # Arrays of credential details
-CREDENTIAL_IDS=("ROX_API_TOKEN" "ROX_CENTRAL_ENDPOINT" "GITOPS_AUTH_PASSWORD" "COSIGN_SECRET_PASSWORD" "COSIGN_SECRET_KEY" "COSIGN_PUBLIC_KEY")
-SECRETS=($ACS_TOKEN $ACS_ENDPOINT $GITOPS_GIT_TOKEN $COSIGN_SECRET_PASSWORD $COSIGN_SECRET_KEY $COSIGN_PUBLIC_KEY)
+CREDENTIAL_IDS=("ROX_API_TOKEN" "ROX_CENTRAL_ENDPOINT" "GITOPS_AUTH_USERNAME" "GITOPS_AUTH_PASSWORD" "COSIGN_SECRET_PASSWORD" "COSIGN_SECRET_KEY" "COSIGN_PUBLIC_KEY")
+SECRETS=($ACS_TOKEN $ACS_ENDPOINT $GITOPS_AUTH_USERNAME $GITOPS_GIT_TOKEN $COSIGN_SECRET_PASSWORD $COSIGN_SECRET_KEY $COSIGN_PUBLIC_KEY)
 
 # Function to add a single credential
-add_credential() {
+add_secret() {
     local id=$1
     local secret=$2
 
@@ -37,27 +40,47 @@ add_credential() {
 }
 EOF
 )
+
+    create_credentials "$json"    
+}
+
+add_username_with_password() {
+    local id=$1
+    local username=$2
+    local password=$3
+
+    local json=$(cat <<EOF
+{
+  "": "0",
+  "credentials": {
+    "scope": "GLOBAL",
+    "id": "${id}",
+    "username": "${username}",
+    "password": "${password}",
+    "description": "",
+    "\$class": "com.cloudbees.plugins.credentials.impl.UsernamePasswordCredentialsImpl"
+  }
+}
+EOF
+)
+
+    create_credentials "$json"
+}
+
+create_credentials() {
+    local json=$1
+
     curl -X POST "$JENKINS__URL/credentials/store/system/domain/_/createCredentials" \
     --user "$JENKINS__USERNAME:$JENKINS__TOKEN" \
     --data-urlencode "json=$json"
 }
 
-curl -X POST "$JENKINS__URL/credentials/store/system/domain/_/createCredentials" \
---user "$JENKINS__USERNAME:$JENKINS__TOKEN" \
---data-urlencode 'json={
-  "": "0",
-  "credentials": {
-    "scope": "GLOBAL",
-    "id": "QUAY_IO_CREDS",
-    "username": "<Quay username>",
-    "password": "<Quay password>",
-    "description": "",
-    "$class": "com.cloudbees.plugins.credentials.impl.UsernamePasswordCredentialsImpl"
-  }
-}'
-
 
 # Add multiple credentials
 for i in "${!CREDENTIAL_IDS[@]}"; do
-    add_credential "${CREDENTIAL_IDS[$i]}" "${SECRETS[$i]}"
+    add_secret "${CREDENTIAL_IDS[$i]}" "${SECRETS[$i]}"
 done
+
+# Add usernames with passwords
+add_username_with_password "QUAY_IO_CREDS" $QUAY_USERNAME $QUAY_PASSWORD
+add_username_with_password "GITOPS_CREDENTIALS" $GITOPS_AUTH_USERNAME $GITOPS_GIT_TOKEN
